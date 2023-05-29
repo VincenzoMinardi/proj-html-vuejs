6fe07762553e1d1ae9839204438540d03047cd18
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-29 15:05:10 +0200

    add: image

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+}
+</style>
