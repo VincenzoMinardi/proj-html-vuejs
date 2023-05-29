146dc4020d1950965899003928f2fa4c4c5aa0d7
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-29 14:54:51 +0200

    add: Header,Main and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
 
-
-export default{
-
-}
-
+export default {
+  AppHeader,
+  AppMain,
+  AppFooter,
+};
 </script>
 
 <template>
-
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style>
-  @import "../node_modules/bootstrap/scss/bootstrap";
+@import "../node_modules/bootstrap/scss/bootstrap";
 </style>
