07920d4021cd6cbe18ea83adf32a8cf3da77c774
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-30 12:26:06 +0200

    add: footer and style main

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,70 @@
-<script></script>
+<script>
+import { store } from "../store";
 
-<template></template>
+export default {
+    data() {
+        return {
+            store,
+        };
+    },
+};
+</script>
 
-<style lang="scss" scoped></style>
+<template>
+    <div class="container">
+        <div v-for="element in store.ArrFooter" :key="element">
+            <div class="container-small">
+                <div>
+                    <h1>{{ element.title }}</h1>
+                    <p>{{ element.paragraf }}</p>
+                </div>
+                <div>
+                    <div>Comment</div>
+                    <hr />
+                </div>
+            </div>
+            <div class="input">
+                <div>Name</div>
+                <hr>
+                <div>
+                    <div>Email</div>
+                    <hr>
+                </div>
+            </div>
+            <button>Submit</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    max-width: 1350px;
+    margin: auto;
+    padding: 1rem;
+
+    .container-small {
+        display: flex;
+        margin-top: 6rem;
+        gap: 10rem;
+    }
+}
+
+hr {
+    width: 500px;
+}
+
+.input,
+button {
+    margin-left: 51rem;
+    margin-bottom: 2rem;
+}
+
+button {
+    border-radius: 3rem;
+    margin-top: 2rem;
+    height: 50px;
+    width: 160px;
+    border-color: #69a9ae;
+    background-color: white;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,64 +12,68 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div v-for="element in store.ArrMain" :key="element" class="title-paragraf">
-      <h1 class="title">{{ element.title }}</h1>
-      <h3 class="paragraf">{{ element.paragraf }}</h3>
-    </div>
-    <img src="../assets/img/hover-block.png" alt="" />
-    <div>
-      <img class="size" src="../assets/img/port-img-09.jpg" alt="" />
-      <img class="size" src="../assets/img/port-img-10.jpg" alt="" />
-      <img class="size" src="../assets/img/port-img-11.jpg" alt="" />
-      <img class="size" src="../assets/img/port-img-12.jpg" alt="" />
-    </div>
-    <div class="cont-flex" v-for="element in store.ArrMain" :key="element">
-      <div class="below-container">
-        <h1>{{ element.belowTitle }}</h1>
-        <div>{{ element.belowParagraf }}</div>
-        <button class="bt-read">Read more</button>
+  <div class="big-image">
+    <div class="container">
+      <div v-for="element in store.ArrMain" :key="element" class="title-paragraf">
+        <h1 class="title">{{ element.title }}</h1>
+        <h3 class="paragraf">{{ element.paragraf }}</h3>
       </div>
-      <div class="cont-flex">
-        <img class="size-img" src="../assets/img/h2-icon-01.png" alt="" />
-        <h2>{{ element.belowTextOne }}</h2>
-        <img src="../assets/img/h2-icon-02.png" alt="" />
-        <h2>{{ element.belowTextTwo }}</h2>
+      <img src="../assets/img/hover-block.png" alt="" />
+      <div>
+        <img class="size" src="../assets/img/port-img-09.jpg" alt="" />
+        <img class="size" src="../assets/img/port-img-10.jpg" alt="" />
+        <img class="size" src="../assets/img/port-img-11.jpg" alt="" />
+        <img class="size" src="../assets/img/port-img-12.jpg" alt="" />
+      </div>
+      <div class="cont-flex" v-for="element in store.ArrMain" :key="element">
+        <div class="below-container">
+          <h1>{{ element.belowTitle }}</h1>
+          <div>{{ element.belowParagraf }}</div>
+          <button class="bt-read">Read more</button>
+        </div>
+        <div class="cont-flex">
+          <img class="size-img" src="../assets/img/h2-icon-01.png" alt="" />
+          <h2>{{ element.belowTextOne }}</h2>
+          <img src="../assets/img/h2-icon-02.png" alt="" />
+          <h2>{{ element.belowTextTwo }}</h2>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="cont-big-image">
-    <img class="big-image" src="../assets/img/h2-img-2.jpg" alt="" />
-    <img
-      class="video-btn"
-      src="../assets/img/main-home-video-button-img-01.png"
-      alt=""
-    />
-  </div>
-  <div v-for="card in store.ArrCard" :key="card" class="cont-card">
-    <div class="card">
-      <h1>{{ card.title }}</h1>
-      <h2 class="price">{{ card.price }}</h2>
-      <p class="text">{{ card.text }}</p>
+    <div class="cont-big-image">
+      <img class="big-image" src="../assets/img/h2-img-2.jpg" alt="" />
+      <img class="video-btn" src="../assets/img/main-home-video-button-img-01.png" alt="" />
     </div>
-    <div class="card">
-      <h1>{{ card.titleTwoCard }}</h1>
-      <h2 class="price">{{ card.priceTwo }}</h2>
-      <p class="text">{{ card.text }}</p>
-    </div>
-    <div class="card">
-      <h1>{{ card.title }}</h1>
-      <h2 class="price">{{ card.priceThree }}</h2>
-      <p class="text">{{ card.text }}</p>
+    <div v-for="card in store.ArrCard" :key="card" class="cont-card">
+      <div class="card">
+        <h1>{{ card.title }}</h1>
+        <h2 class="price">{{ card.price }}</h2>
+        <p class="text">{{ card.text }}</p>
+      </div>
+      <div class="card">
+        <h1>{{ card.titleTwoCard }}</h1>
+        <h2 class="price">{{ card.priceTwo }}</h2>
+        <p class="text">{{ card.text }}</p>
+      </div>
+      <div class="card">
+        <h1>{{ card.title }}</h1>
+        <h2 class="price">{{ card.priceThree }}</h2>
+        <p class="text">{{ card.text }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.big-image {
+  background-image: url(../assets/img/h2-background-img-02.png);
+  background-repeat: repeat-y;
+}
+
 .container {
   max-width: 1350px;
   margin: auto;
   padding: 1rem;
+
 
   .title-paragraf {
     display: flex;
@@ -85,6 +89,7 @@ export default {
     margin-top: 3rem;
     color: lightgrey;
   }
+
   .size {
     height: 670px;
     width: 651px;
@@ -119,6 +124,7 @@ export default {
 
 .cont-big-image {
   position: relative;
+
   .big-image {
     margin-top: 6rem;
     width: 100%;
